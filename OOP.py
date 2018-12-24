667211673a9c4c75905696efb49b7be2e3ef1b0e
breadream <brad.dh.lim@gmail.com>
2018-12-24 15:17:42 -0600

    Added some stuff

--- OOP.py
+++ OOP.py
@@ -1,3 +1,17 @@
+import pygame
+import random
+
+
+WIDTH = 800
+HEIGHT = 600
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+
+game_display = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Blob World")
+clock = pygame.time.Clock()
+
 class Blob:
 
     def __init__(self, color):
@@ -17,4 +31,28 @@ class Blob:
 
         if self.y < 0: self.y = 0
         elif self.y > HEIGHT: self.y = HEIGHT 
+
+
+def draw_environment(blob):
+    game_display.fill(WHITE)
+    pygame.draw.circle(game_display, blob.color, [blob.x, blob.y], blob.size)
+    pygame.display.update()
+    blob.move()
+
+
+def main():
+    red_blob = Blob(RED)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        draw_environment(red_blob) 
+        clock.tick(60)
+        print(red_blob.x, red_blob.y)
+
+
+if __name__ == '__main__':
+    main()
 
